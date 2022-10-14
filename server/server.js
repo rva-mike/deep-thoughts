adaf3b02117e5fb6f9694b6fac255bd70b99a43f
rva-mike <mike.levy.rva@gmail.com>
2022-10-14 10:57:10 -0400

    set up mutations and JWT logic

--- server/server.js
+++ server/server.js
@@ -8,16 +8,22 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+// import middleware function
+const { authMiddleware } = require('./utils/auth');
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
